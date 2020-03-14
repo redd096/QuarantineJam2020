16cef124e80247d3abfa74e3221c09bf2d960eec
Willy Utano <35328288+hyunkelw@users.noreply.github.com>
2020-03-14 08:34:54 +0100

    First setup of the Main Scene
    WARNING: There's some stuff in the scene and in the scripts that needs to be removed

---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LevelTimer : MonoBehaviour
+{
+    [Tooltip("Level time in seconds")]
+    [SerializeField] float baseTime = 10f;
+    [SerializeField] float preparationTime = 30f;
+    [SerializeField] Text countdownTimerText;
+
+    bool triggeredLevelFinish;
+    bool levelStarted;
+
+    // cached references
+    Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggeredLevelFinish) { return; }
+        if (!levelStarted)
+        {
+            CountdownToStart();
+        }
+        else
+        {
+            UpdateLevelTimer();
+        }
+    }
+
+    private void UpdateLevelTimer()
+    {
+        slider.value = (Time.timeSinceLevelLoad - preparationTime) / baseTime;
+        bool timerFinished = (Time.timeSinceLevelLoad >= baseTime);
+        if (timerFinished)
+        {
+            FindObjectOfType<LevelController>().LevelTimerFinished();
+            triggeredLevelFinish = true;
+            GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    private void CountdownToStart()
+    {
+        preparationTime -= 1 * Time.deltaTime;
+        countdownTimerText.text = preparationTime.ToString("00");
+        if (preparationTime <= 0)
+        {
+            levelStarted = true;
+            //GetComponentInChildren<Canvas>().enabled = false;
+            //var enemySpawners = FindObjectsOfType<AttackerSpawner>();
+            //foreach(AttackerSpawner spawner in enemySpawners)
+            //{
+            //    spawner.StartSpawning();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScreenLoader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenLoader : MonoBehaviour
+{
+    [SerializeField] float delaySeconds = 3.5f;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitAndLoad());
+        }
+    }
+
+    public void ReloadLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void LoadOptionsMenu()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadYouLose()
+    {
+        SceneManager.LoadScene("Lose Screen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
