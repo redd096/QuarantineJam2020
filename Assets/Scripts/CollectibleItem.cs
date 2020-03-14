a59ce3cd6d0d1df7cb04024e6e290fcf28423bdb
Alessandro Tironi <alessandro.tironi7@gmail.com>
2020-03-14 12:20:22 +0100

    Merge branch 'game-manager'

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaranteam
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CollectibleItem : MonoBehaviour
+    {
+        private SpriteRenderer sprite;
+        private BoxCollider2D boxCollider;
+        private Rigidbody2D rb;
+
+        protected internal float reward;
+
+        public void LoadFromShoppingItem(ShoppingItem item)
+        {
+            sprite = GetComponent<SpriteRenderer>();
+            boxCollider = GetComponent<BoxCollider2D>();
+            rb = GetComponent<Rigidbody2D>();
+
+            sprite.sprite = item.Sprite;
+            rb.mass = item.Weight;
+            reward = item.BaseReward;
+        }
+    }
+}
+
---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Quaranteam
+{
+    [RequireComponent(typeof(Slider))]
+    public class GameManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Fill the rules of this level.
+        /// </summary>
+        public GameRules appliedGameRules;
+
+        LevelTimer levelTimer;
+
+        /// <summary>
+        /// The update function for each state.
+        /// </summary>
+        private List<Action> updateFunctions;
+
+        private void Awake()
+        {
+
+            //updateFunctions.Add(WaitForGameStart);
+            //updateFunctions.Add(ManagePreparationTime);
+            //updateFunctions.Add(GameUpdate);
+
+            Debug.Log("Press Enter to start the game");
+
+            StartCoroutine(WaitForEnterButton());
+        }
+
+        private IEnumerator WaitForEnterButton()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                levelTimer.gameObject.SetActive(true);
+            }
+
+            yield return null;
+        }
+    }
+}
+
