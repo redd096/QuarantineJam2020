b1d9494c874558b7d02d957b4eb96f6425a27a4c
redd096 <martin.babino96@gmail.com>
2020-03-16 19:04:00 +0100

    crediti e cambiato nome in Survival Spesa

---
+++ Assets/Scripts/CreditsButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsButton : MonoBehaviour
+{
+    [SerializeField] GameObject credits;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ShowHideCredits);
+    }
+
+    public void ShowHideCredits()
+    {
+        credits.SetActive(!credits.activeSelf);
+    }
+}
