e3f91b5dc956e21911baab10a22dfe6e9ab14197
Willy Utano <35328288+hyunkelw@users.noreply.github.com>
2020-03-16 16:21:36 +0100

    With Scriptable Assets

--- Assets/Scripts/ScreenLoader.cs
+++ Assets/Scripts/ScreenLoader.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class ScreenLoader : MonoBehaviour
 {
-    [SerializeField] float delaySeconds = 5f;
+    [SerializeField] float delaySeconds = 3.5f;
     int currentSceneIndex;
 
     private void Start()
@@ -15,12 +15,43 @@ public class ScreenLoader : MonoBehaviour
             StartCoroutine(WaitAndLoad());
         }
     }
+
+    public void ReloadLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void LoadOptionsMenu()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
     private IEnumerator WaitAndLoad()
     {
-
         yield return new WaitForSeconds(delaySeconds);
-        GetComponent<Animator>().SetTrigger("To_Fade_OUT");
-        yield return new WaitForSeconds(3f);
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadYouLose()
+    {
+        SceneManager.LoadScene("Lose Screen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
